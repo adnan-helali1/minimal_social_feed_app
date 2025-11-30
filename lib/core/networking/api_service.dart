4e19ee1ba84cf0404f61e9f7dfbf5574aac9a4c4
adnan-helali1 <helaliadnan877@gmail.com>
2025-11-30 21:13:47 +0300

    fix api service

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:minimal_social_feed_app/core/networking/api_constans.dart';
 import 'package:minimal_social_feed_app/features/feed/data/models/feed_response_model.dart';
-import 'package:minimal_social_feed_app/features/feed/data/models/feed_response_model.dart';
 import 'package:minimal_social_feed_app/features/login/data/models/login_request_body.dart';
 import 'package:minimal_social_feed_app/features/login/data/models/login_response.dart';
 import 'package:minimal_social_feed_app/features/register/data/models/register_request_body.dart';
 import 'package:minimal_social_feed_app/features/register/data/models/register_response.dart';
 import 'package:minimal_social_feed_app/features/single_post/data/models/single_post_response_model.dart';
+
 import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
